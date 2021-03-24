100aa48eaa4b7a95528b78526c04c5484bca5d6a
Fernando Forero <6925409+nando-nando-nando@users.noreply.github.com>
2021-03-23 23:47:52 -0600

    Added browser disguise to urllib agent

--- modules/filehandler.py
+++ modules/filehandler.py
@@ -15,7 +15,18 @@ logger = logging.getLogger(__name__)
 # Saves the CSV to local storage
 # Most ETFs publish their holdings as CSVs
 def collectcsv(file_location, url):
-    urllib.request.urlretrieve(url, file_location)  # For Python 3
+    headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'}
+    request_=urllib.request.Request(url,None,headers) #The assembled request
+    response = urllib.request.urlopen(request_)# store the response
+    #create a new file and write the image
+    f = open(file_location,'wb')
+    f.write(response.read())
+    f.close()
 
 # Returns the workbook for the last day recorded and the date in that book
 # today: pass datetime.now()
